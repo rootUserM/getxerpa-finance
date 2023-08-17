17d4609a64cc17826d082b702fcf92c1aa2518d9
Mizael V <misael_v_g@outlook.com>
2023-08-16 23:13:10 -0600

    [Update] Serialaizers modified

--- back-getxerpa/getxerpa/finance/serializers.py
+++ back-getxerpa/getxerpa/finance/serializers.py
@@ -24,76 +24,50 @@ class CategoryExpensesSerializer(serializers.ModelSerializer):
     percent_expensed = serializers.SerializerMethodField()
     count_transactions = serializers.SerializerMethodField()
     amount_avilable = serializers.SerializerMethodField()
+
+    def get_common_totals(self, obj):
+        transactions = Transaction.objects.filter(category=obj)
+        total = sum([transaction.amount for transaction in transactions])
+        return total
+
     def get_total_expenses(self, obj):
-        if obj.limit > 0:
-            transactions = Transaction.objects.filter(category=obj)
-            total = sum([transaction.amount for transaction in transactions])
-            self.get_percent_expensed
-            return total
-        return None
+        return self.get_common_totals(obj)
 
     def get_percent_expensed(self, obj):
         if obj.limit > 0:
-            transactions = Transaction.objects.filter(category=obj)
-            total = sum([transaction.amount for transaction in transactions])
-            percent = total/obj.limit * 100
+            total = self.get_common_totals(obj)
+            percent = total / obj.limit * 100
             return percent
         return None
-    
+
     def get_count_transactions(self, obj):
-        if obj.limit == 0:
-            transactions = Transaction.objects.filter(category=obj).count()
-            return transactions
-        return None
-    
+        transactions_count = Transaction.objects.filter(category=obj).count()
+        return transactions_count
+
     def get_amount_avilable(self, obj):
         if obj.limit == 0:
-            transactions = Transaction.objects.filter(category=obj)
-            total = sum([transaction.amount for transaction in transactions])
+            total = self.get_common_totals(obj)
             am_available = total - obj.limit
             return am_available
         return None
-    
+
     class Meta:
         model = Category
         fields = '__all__'
 
-class CategoryExpensesDetailSerializer(serializers.ModelSerializer):
-    total_expenses = serializers.SerializerMethodField()
-    percent_expensed = serializers.SerializerMethodField()
-    count_transactions = serializers.SerializerMethodField()
-    amount_avilable = serializers.SerializerMethodField()
-    def get_total_expenses(self, obj):
-        transactions = Transaction.objects.filter(category=obj)
-        total = sum([transaction.amount for transaction in transactions])
-        self.get_percent_expensed
-        return total
-       
 
-    def get_percent_expensed(self, obj):
-        if obj.limit > 0:
-            transactions = Transaction.objects.filter(category=obj)
-            total = sum([transaction.amount for transaction in transactions])
-            percent = total/obj.limit * 100
-            return percent
-        return None
-    
-    def get_count_transactions(self, obj):
-        transactions = Transaction.objects.filter(category=obj).count()
-        return transactions
-       
-    
+class CategoryExpensesDetailSerializer(CategoryExpensesSerializer):
     def get_amount_avilable(self, obj):
         if obj.limit > 0:
-            transactions = Transaction.objects.filter(category=obj)
-            total = sum([transaction.amount for transaction in transactions])
+            total = self.get_common_totals(obj)
             am_available = obj.limit - total
             return am_available
         return None
-    
-    class Meta:
+
+    class Meta(CategoryExpensesSerializer.Meta):
         model = Category
         fields = '__all__'
+
 
 
 
